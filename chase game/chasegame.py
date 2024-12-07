c1ffcfecaa4cb59fd11d8dc7fff17546fc044835
Amelia-code123 <Sarahnicoleknight@gmail.com>
2024-12-07 16:57:32 +0000

    chasegame.py

--- chase game/chasegame.py
+++ chase game/chasegame.py
@@ -4,6 +4,7 @@ HEIGHT=640
 
 octopus=Actor("octopus.png")
 fish=Actor("small fish.png")
+fish.pos=(350,320)
 
 def draw():
     screen.blit("underwater background",(0,0))
@@ -12,5 +13,19 @@ def draw():
 
 def update():
     pass
+    if keyboard.left:
+        octopus.x-=5
+    if keyboard.right:
+        octopus.x+=5
+    if keyboard.up:
+        octopus.y-=5
+    if keyboard.down:
+        octopus.y+=5
+    if octopus.colliderect(fish):
+        coordinates()
+
+def coordinates():
+    fish.x=random.randint(50,650)
+    fish.y=random.randint(50,600)
 
 pgzrun.go()
