bbf2192776e1d373421adbf38eb9384cd74bd29a
Amelia-code123 <Sarahnicoleknight@gmail.com>
2025-03-29 16:44:09 +0000

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,20 @@
+t=1,2,3,4,5
+print(t)
+print(type(t))
+print(t[-4])
+#l=list(t)
+#modifying is not possible
+
+x=7,8,9,10,1
+#concatination
+print(t+x)
+
+#repetition
+print(t*3)
+
+#membership
+print(5 in t)
+
+#deleting
+del x
+print(x)
