82464c9d327cd44c5482617bfd90f2742cad3b02
Amelia-code123 <Sarahnicoleknight@gmail.com>
2025-04-26 16:20:46 +0100

    operations.py

--- operations.py
+++ operations.py
@@ -2,4 +2,17 @@ s=set(input("Enter the set elements separated by a space: "))
 ss=set(input("Enter the second set elements separated by a space: "))
 print(s)
 print(ss)
-print("MENU: \n 1.Union \n 2.Intersection \n 3.Difference \n 4.Symetric Difference")
+while True:
+    print("MENU: \n 1.Union \n 2.Intersection \n 3.Difference \n 4.Symetric Difference \n 5.Exit")
+    choice=int(input("Choose an option: "))
+
+    if choice==1:
+        print(s|ss)
+    elif choice==2:
+        print(s&ss)
+    elif choice==3:
+        print(s-ss)
+    elif choice==4:
+        print(s^ss)
+    elif choice==5:
+        break
